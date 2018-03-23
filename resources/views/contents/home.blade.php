b237562d4a6f741af7d0959e37db3216a82575dc
Jeanine Schoessler <jeanine.mt@gmail.com>
2018-03-23 17:40:36 -0600

    small style updates

--- resources/views/contents/home.blade.php
+++ resources/views/contents/home.blade.php
@@ -48,23 +48,23 @@
 <h2 id="subfeature">Creating life experiences</h2>
 <p class="lead">How we help you build connections: </p>
 <div class="row subfeature">
-  <div class="col-lg-4 col-md-6 text-center">
+  <div class="col-lg-4 col-md-6">
   <a href="#" aria-label="community"><span class="fas fa-users" aria-hidden="true"></span></a>
     <h3>Community</h3>
     <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-    <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+    <p class="text-center"><a class="btn btn-primary btn-sm" href="#" role="button">View details &raquo;</a></p>
   </div><!-- /.col-lg-4 -->
-  <div class="col-lg-4 col-md-6 text-center">
+  <div class="col-lg-4 col-md-6">
   <a href="#" aria-label="Newspaper"><span class="far fa-newspaper" aria-hidden="true"></span></a>
     <h3>News</h3>
     <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-    <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+    <p class="text-center"><a class="btn btn-primary btn-sm" href="#" role="button">View details &raquo;</a></p>
   </div><!-- /.col-lg-4 -->
-  <div class="col-lg-4 col-md-6 offset-md-3 offset-lg-0 text-center">
+  <div class="col-lg-4 col-md-6 offset-md-3 offset-lg-0 ">
   <a href="#" aria-label="learning"><span class="fas fa-book" aria-hidden="true"></span></a>
     <h3>Learning</h3>
     <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-    <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+    <p class="text-center"><a class="btn btn-primary btn-sm" href="#" role="button">View details &raquo;</a></p>
   </div><!-- /.col-lg-4 -->
 </div><!-- /.row -->
 

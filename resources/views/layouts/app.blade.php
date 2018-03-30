40bc8d1aeccd9adfd4b3b87f5c670f6c277ec184
virtual <jeanine.mt@gmail.com>
2018-03-30 15:53:12 -0600

    format

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -242,43 +242,40 @@
       slickOptions = {
         dots: true,
         arrows: false,
-  infinite: true,
-  autoplay: true,
+        infinite: true,
+        autoplay: true,
         arrows: false,
         speed: 1000,
-        autoplaySpeed: 8000 ,
-  slidesToShow: 1,
-  slidesToScroll: 2,
-  centerMode: true,
-  variableWidth: true,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
+        autoplaySpeed: 8000,
         slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
+        slidesToScroll: 2,
+        centerMode: true,
+        variableWidth: true,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          }, {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          }, {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
       }
       slider.slick(slickOptions);
       $('.slick-slider .caption').each(function() {
